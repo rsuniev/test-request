2754465b45a2fc4a8190dd05a985a26b3544f94d
Rustem Suniev <rustem.suniev@6point6.co.uk>
2016-09-01 15:56:15 +0100

    Switch to async processing

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ var ENV_SLEEP_SECONDS = process.env.ENV_SLEEP_SECONDS || 15;
 var app = express();
 
 app.get('/', function(req, res) {
-  console.log("Sleeping for " + ENV_SLEEP_SECONDS + " seconds");
-  var waitTill = new Date(new Date().getTime() + ENV_SLEEP_SECONDS * 1000);
-  while(waitTill > new Date()){}
-  console.log("Finished waiting");
-  res.send("Hello from the container");
+  setTimeout(function(){
+    console.log("Sleeping for " + ENV_SLEEP_SECONDS + " seconds");
+    res.status(200).send('Hello from the container').end();
+  },ENV_SLEEP_SECOND * 1000);
 });
 
 var server = app.listen(8080, function() {
